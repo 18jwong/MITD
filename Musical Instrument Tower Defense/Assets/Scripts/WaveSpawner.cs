801f430ed8e7aa49ca0a5b9e1cfe714b6011f3e8
Jordan Wong <jotywong@ucsc.edu>
2019-07-12 23:11:50 -0700

    E03-4 - Spawning & Turret Tracking

---
+++ Musical Instrument Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountdownText;
+
+    public float timeBetweenEnemies = 0.5f;
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        waveCountdownText.text = Mathf.Ceil(countdown).ToString();
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
