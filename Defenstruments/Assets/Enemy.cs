b44d0c697a8a561b83366b99bb2c835abed940cf
Jordan Wong <jotywong@ucsc.edu>
2019-08-05 20:26:25 -0700

    Started work on Turrets and Enemies
    +AltoSax
    +QuarterRest
    
    note: no functionality except for rest movement

---
+++ Defenstruments/Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Properties")]
+    public float startSpeed = 10f;
+    public float startHealth = 100f;
+    public int moneyValue = 50;
+    public int livesValue = 1;
+
+    private float speed;
+    private float health;
+
+    private bool isDead = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = startSpeed;
+        health = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead)
+            return;
+
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+    // Manipulation Functions ------------------------------
+    
+    public void DecreaseHealth(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        Destroy(gameObject);
+    }
+}
---
+++ Defenstruments/Assets/Scripts/Tower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Header("Tower Properties")]
+    public float range = 99f;
+    public float damage = 10f;
+
+    public GameObject projectilePrefab;
+    public float shotsPerSecond = 1f;
+    public float initialTimeUntilFire = 1f;
+    private float fireCountdown;
+
+    [Header("Unity Setup Fields")]
+    public Transform firePoint;
+
+    // Private variables
+    private Transform target;
+    private Enemy targetEnemy;
+    private Enemy[] enemiesInLane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireCountdown = initialTimeUntilFire;
+
+        //InvokeRepeating()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Manipulation Functions ------------------------------
+
+    public void AddEnemy(Enemy enemy)
+    {
+
+    }
+}
